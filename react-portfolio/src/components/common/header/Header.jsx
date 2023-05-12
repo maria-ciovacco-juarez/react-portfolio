54dc21e492d50a20b0af67e016f4b09a1034e717
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-05-12 11:23:12 -0500

    added sections and info to header.jsx

--- react-portfolio/src/components/common/header/Header.jsx
+++ react-portfolio/src/components/common/header/Header.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 
-function Header () {
+function Header() {
 
   return (
-    <section>
-      Hello
+    <section className="header">
+      <section className="header-top">
+        <section className="header-top__logo">
+          <a href="/" className="header-logo">MCJ</a>
+        </section>
+        <section className="header-top__navbar">
+          <section className="header-top__navigation">
+            {/* <Navbar /> */}
+          </section>
+          <hr className="header-top__seperator" />
+        </section>
+      </section>
+      <section className="header-bottom">
+        <section className="header-bottom__phone">
+          205.383.6223
+        </section>
+        <section className="header-bottom__email">
+          ciovaccomaria@gmail.
+        </section>
+      </section>
     </section>
   )
 }
