3f2b1b1cb1f6e06b0de8322ed90bcc69d97dd5ce
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-05-20 13:41:53 -0500

    added routes

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './index.css';
+import Home from './routes/Home'
+import About from './routes/About'
+import Contact from './routes/Contact'
+import Projects from './routes/Projects'
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "./components/Navbar"
-
-
-
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
-    <Navbar />
+    // <Navbar />,
+    <>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/projects" element={<Projects />}/>
+      <Route path="/contact" element={<Contact />}/>
+    </Routes>
+    
+   </>
   );
 }
 
