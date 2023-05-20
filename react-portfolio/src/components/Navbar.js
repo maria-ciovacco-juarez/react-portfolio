071b3c513e088868196710d7297ae9ff2fc041be
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-05-20 13:58:14 -0500

    added to components

--- react-portfolio/src/components/Navbar.js
+++ react-portfolio/src/components/Navbar.js
@@ -1,31 +1,59 @@
 import React from 'react'
 
+import "./NavbarStyles.css"
+import { Link } from "react-router-dom"
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-<div className='container'> 
-
-  <a className="navbar-brand" href="#">MCJ</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only"></span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
-      </li>
-      
-    </ul>
-    
-  </div>
-</div>
-</nav>
+    <div className="header">
+      <Link to="/">
+        <h1>Portfolio</h1>
+      </Link>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/project">Projects</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
   )
 }
 
-
 export default Navbar
+
+// const Navbar = () => {
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+// <div className='container'> 
+
+//   <a className="navbar-brand" href="#">MCJ</a>
+//   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//     <span className="navbar-toggler-icon"></span>
+//   </button>
+
+//   <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//     <ul className="navbar-nav mr-auto">
+//       <li className="nav-item active">
+//         <a className="nav-link" href="#">Home <span className="sr-only"></span></a>
+//       </li>
+//       <li className="nav-item">
+//         <a className="nav-link" href="#">Portfolio</a>
+//       </li>
+      
+//     </ul>
+    
+//   </div>
+// </div>
+// </nav>
+//   )
+// }
+
+
+// export default Navbar
