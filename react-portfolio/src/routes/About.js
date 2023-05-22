db32e72a52f2391752f237c6af3c0006782d7625
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-05-22 16:53:03 -0500

    added data to project.js and styling

--- react-portfolio/src/routes/About.js
+++ react-portfolio/src/routes/About.js
@@ -4,12 +4,14 @@ import Navbar from '../components/Navbar'
 
 import Footer from "../components/Footer";
 import HeroImg2 from '../components/HeroImg2';
+import AboutContent from '../components/AboutContent';
 
 const About = () => {
   return <div>
-  <Navbar/>
-  <HeroImg2 heading="ABOUT." text="I'm a friendly Front-End Developer."/>
-  <Footer/>
+    <Navbar />
+    <HeroImg2 heading="ABOUT ME" text="I'm a friendly Front-End Developer." />
+    <AboutContent/>
+    <Footer />
   </div>
 }
 
