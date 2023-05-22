6fa2a1dff26f5568b0e5d19e20a815c13015a0ea
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-05-22 10:45:30 -0500

    added footer to render on all pages.

--- react-portfolio/src/routes/Projects.js
+++ react-portfolio/src/routes/Projects.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import Navbar from '../components/Navbar'
+import HeroImg2 from "../components/HeroImg2"
+import Footer from "../components/Footer";
+
+
 const Project = () => {
-  return (
-    <div>Project</div>
-  )
+  return <div>
+    <Navbar/>
+    <HeroImg2/>
+    <Footer/>
+    </div>
+  
 }
 
 export default Project
