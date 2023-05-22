9d8271d23e69101c5bb621eab12d00c7f444c10b
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-05-22 18:03:46 -0500

    added to about page

--- react-portfolio/src/components/AboutContent.js
+++ react-portfolio/src/components/AboutContent.js
@@ -1,10 +1,29 @@
 import "./AboutContentStyles.css"
-
+import {Link} from "react-router-dom";
 import React from 'react'
+import React1 from "../assets/react1.jpg"
+import React2 from "../assets/react2.jpg"
 
 const AboutContent = () => {
   return (
-    <div>AboutContent</div>
+    <div className="about">
+      <div className="left">
+        <h1> Who Am I?</h1>
+        <p>Currently, I am a full-stack developer student at the University of Minnesota, and I have plans to obtain my certification. I have knowledge in Markup language, JavaScript, React, and SQL. I'm excited to connect with you!</p>
+        <Link to="/contact">
+          <button className="btn">Contact</button>
+        </Link>
+      </div>
+      <div className="right"></div>
+      <div className="img-container">
+        <div className="img-stack top">
+          <img src={React1} className="img" alt="true"/>
+        </div>
+        <div className="img-stack top">
+          <img src={React2} className="img" alt="true"/>
+        </div>
+      </div>
+    </div>
   )
 }
 
