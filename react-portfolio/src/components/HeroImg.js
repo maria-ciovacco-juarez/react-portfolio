a8ec6fd64885a048a89b48ac709ec55c2b526f6d
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-05-21 20:27:38 -0500

    added assets, hero img, and styling

---
+++ react-portfolio/src/components/HeroImg.js
@@ -0,0 +1,29 @@
+import "./HeroImgStyles.css";
+
+import React from 'react';
+
+import IntroImg from "../assets/intro-bg.jpg"
+
+import {Link} from "react-router-dom"
+
+const HeroImg = () => {
+  return (
+  <div className="hero">
+    <div className="mask">
+      <img className="intro-img" src={IntroImg} alt="IntroImg"/>
+
+    </div>
+    <div className="content">
+      <p>MARIA CIOVACCO JUAREZ</p>
+      <h1>Full Stack Developer.</h1>
+      <div>
+        <Link to="/project"
+        className="btn">Projects</Link>
+      </div>
+    </div>
+  </div>
+  );
+  
+};
+
+export default HeroImg;
--- react-portfolio/src/routes/Home.js
+++ react-portfolio/src/routes/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import HeroImg from '../components/HeroImg';
+
 
 const Home = () => {
   return (
-    <Navbar/>
+    <div>
+      <Navbar/>
+      <HeroImg/>
+    </div>
   )
 }
 
-export default Home
+export default Home;
