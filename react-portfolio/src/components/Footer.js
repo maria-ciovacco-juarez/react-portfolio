abb6793c42c37345bb0ba17113e1e04d8304640b
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-05-21 21:47:29 -0500

    added footer description and styling

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -2,7 +2,15 @@ import "./FooterStyles.css"
 
 import React from 'react'
 
-import {FaHome} from "react-icons/fa";
+import {FaHome, FaGithub, FaMailBulk, FaTwitter, FaInstagram} from "react-icons/fa";
+
+// import { FaMailBulk } from "react-icons/fa";
+
+// import { FaTwitter } from "react-icons/fa";
+
+// import { FaInstagram } from "react-icons/fa";
+
+
 
 const Footer = () => {
   return (
@@ -10,9 +18,19 @@ const Footer = () => {
       <div className="footer-container">
         <div className="left"></div>
         <div className="location">
-          <FaHome size={20} style ={{color:"#fff", marginRight: "2rem" }}/>
+          <h4><FaHome size={20} style ={{color:"#fff", marginRight: "2rem" }}/>Grand Prairie, Texas
+          </h4>
+          <div className="email">
+          <h4><FaMailBulk size={20} style ={{color:"#fff", marginRight: "2rem" }}/>ciovaccomaria@gmail.com</h4>
+          </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="social">
+          <FaGithub size={20} style ={{color:"#fff", marginRight: "2rem" }}/>
+          <FaTwitter size={20} style ={{color:"#fff", marginRight: "2rem" }}/>
+          <FaInstagram size={20} style ={{color:"#fff", marginRight: "2rem" }}/>
+          </div>
+        </div>
       </div>
     </div>
   )
